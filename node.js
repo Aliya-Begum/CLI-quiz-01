a50139e22d439c822c7c62705ce2514901ffe562
aliya begum <111738881+Aliya-Begum@users.noreply.github.com>
2022-08-28 08:22:44 +0000

    created a cli quiz-01

--- node.js
+++ node.js
@@ -1,1 +1,45 @@
+var readlineSync = require("readline-sync");
+var score = 0;
+var userName = readlineSync.question("Let me know your name ");
+console.log("Welcome " +userName);
+console.log("Lets see how much you know Me"); 
+console.log("Let's Start");
+ function play(question, answer){
+   var userAnswer = readlineSync.question(question);
+  
+   if(userAnswer.toUpperCase() === answer){
+     console.log("YAY! you got it right");
+     score = score+1;
+   }else{
+     console.log("Oops! you got it wrong");
+    
+   } 
+ }
+var questionList = [{
+  question: "What is my full name? ",
+  answer : "ALIYA BEGUM"
+}, 
+ {
+  question : "where do I live? ",   
+  answer : "ETAWAH"
+ },{
+  question : "Where do I work? ",
+  answer : "INFOSYS"
+},
+{
+  question : "Which dessert I love most? ",
+  answer : "CAKE"
+},
+{
+  question : "When is my birthday? ",
+  answer : "NOV"
+}
+]
+for (let i = 0; i < 5; i++) {
+  var currentQuestion = questionList[i];
+  play(currentQuestion.question , currentQuestion.answer);
+  console.log("-------");
+  
+}
+console.log("Your final score is : " +score);
 
